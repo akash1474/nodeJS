e96d35d2fc024bc9156836ba9881d6ebdeb18677
akash1474 <panditakash38@gmail.com>
2020-04-16 15:31:05 +0530

    Initial Commit

--- utils/email.js
+++ utils/email.js
@@ -3,10 +3,7 @@ const sendEmail=options=>{
     //1]CREATE A TRANSPORTER
     const transporter=nodemailer.createTransport({
         service:'Gmail',
-        auth:{
-            user:
-        }
-    })
+        
     //2]DEFINE EMAIL OPTIONS
 
     //3]SEND THE EMAIL WITH NODEMAILER
