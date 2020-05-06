42164e5c8a4c30dd50ff6369614765e0f6e98741
akash1474 <panditakash38@gmail.com>
2020-05-06 16:02:58 +0530

    removed the localhost from the server

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ mongoose
   });
 
 const port = process.env.PORT || 3000;
-const server = app.listen(port, 'localhost', () => {
+const server = app.listen(port, () => {
   console.log(`App running on port:${port}`);
 });
 //Unhandled Rejection
